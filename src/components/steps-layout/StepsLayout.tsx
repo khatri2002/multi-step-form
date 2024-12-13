c6d93d1953bdd87c0ce64b80a7b47e8c6d71acc6
Jay Khatri <jay.khatri@oorjan.com>
2024-12-13 15:39:05 +0530

    feat(responsive layout implemented)

--- src/components/steps-layout/StepsLayout.tsx
+++ src/components/steps-layout/StepsLayout.tsx
@@ -15,41 +15,86 @@ const StepsLayout = ({
 }: StepsLayoutProps) => {
   const { activeStep, handleSetActiveStep } = UseDataContext();
   return (
-    <div
-      className={classNames({
-        [styles.container]: true,
-        [styles.hide]: hide,
-      })}
-    >
-      <h2 className={styles.title}>{title}</h2>
-      <span className={styles.desc}>{desc}</span>
-      <form className={styles.form} onSubmit={onSubmit}>
-        {children}
-        <div className={styles.btnContainer}>
-          <button
+    <>
+      <div
+        className={classNames({
+          [styles.container]: true,
+          [styles.hide]: hide,
+        })}
+      >
+        <h2 className={styles.title}>{title}</h2>
+        <span className={styles.desc}>{desc}</span>
+        <form className={styles.form} onSubmit={onSubmit}>
+          {children}
+          <div
             className={classNames({
-              [styles.back]: true,
-              [styles.hide]: hideBackBtn,
+              [styles.btnContainer]: true,
+              [styles.desktop]: true,
             })}
-            onClick={(e) => {
-              e.preventDefault();
-              handleSetActiveStep(activeStep - 1);
-            }}
           >
-            Go Back
+            <button
+              className={classNames({
+                [styles.back]: true,
+                [styles.hide]: hideBackBtn,
+              })}
+              onClick={(e) => {
+                e.preventDefault();
+                handleSetActiveStep(activeStep - 1);
+              }}
+            >
+              Go Back
+            </button>
+            {showConfirmBtn ? (
+              <button
+                disabled={loading}
+                type="submit"
+                className={styles.confirm}
+              >
+                Confirm
+              </button>
+            ) : (
+              <button type="submit" className={styles.next}>
+                Next Step
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+      <div
+        className={classNames({
+          [styles.btnContainer]: true,
+          [styles.mobile]: true,
+          [styles.hide]: hide,
+        })}
+      >
+        <button
+          className={classNames({
+            [styles.back]: true,
+            [styles.hide]: hideBackBtn,
+          })}
+          onClick={(e) => {
+            e.preventDefault();
+            handleSetActiveStep(activeStep - 1);
+          }}
+        >
+          Go Back
+        </button>
+        {showConfirmBtn ? (
+          <button
+            disabled={loading}
+            onClick={onSubmit}
+            type="submit"
+            className={styles.confirm}
+          >
+            Confirm
           </button>
-          {showConfirmBtn ? (
-            <button disabled={loading} type="submit" className={styles.confirm}>
-              Confirm
-            </button>
-          ) : (
-            <button type="submit" className={styles.next}>
-              Next Step
-            </button>
-          )}
-        </div>
-      </form>
-    </div>
+        ) : (
+          <button type="submit" onClick={onSubmit} className={styles.next}>
+            Next Step
+          </button>
+        )}
+      </div>
+    </>
   );
 };
 
