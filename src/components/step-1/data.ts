d69c4a77e6fa3f59500c19c5957db2cf2ee8ba6a
Jay Khatri <jay.khatri@oorjan.com>
2024-12-13 16:05:10 +0530

    feat(step1 extra validations added)

--- src/components/step-1/data.ts
+++ src/components/step-1/data.ts
@@ -11,6 +11,9 @@ export const data: Data = {
       type: "text",
       rules: {
         required: { value: true, message: "This field is required" },
+        pattern: { value: /^[A-Za-z\s]+$/i, message: "Valid name is required" },
+        minLength: { value: 2, message: "Valid name is required" },
+        maxLength: { value: 25, message: "Valid name is required" },
       },
     },
     {
@@ -20,6 +23,10 @@ export const data: Data = {
       type: "text",
       rules: {
         required: { value: true, message: "This field is required" },
+        pattern: {
+          value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+          message: "Valid email is required",
+        },
       },
     },
     {
@@ -29,6 +36,11 @@ export const data: Data = {
       type: "text",
       rules: {
         required: { value: true, message: "This field is required" },
+        pattern: {
+          value: /^[0-9+\s]+$/,
+          message: "Valid phone number is required",
+        },
+        minLength: { value: 10, message: "Valid phone number is required" },
       },
     },
   ],
